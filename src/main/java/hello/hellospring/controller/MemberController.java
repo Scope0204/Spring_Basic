359890170d60f2b6ac707e83d9fe3d4691acc389
Scope0204 <scope0204@gmail.com>
2022-01-21 21:30:49 +0900

    회원 관리 예제 - 웹 MVC 개발

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,6 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 컴포넌트 스캔 방식
 //@Controller -> @Component이 등록 되어있다.
@@ -29,4 +34,30 @@ public class MemberController {
         this.memberService = memberService;
     }
     */
+
+    // Get: url에서 접속, 보통 조회할 때
+    // Post : form에서 전달받을 때
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers(); // member의 리스트를 전부 반환
+        model.addAttribute("members", members); // members는 모든회원을 조회해서 담아둔 형태임
+        return "members/memberList";
+    }
 }
